3f2a147a269410439a2c619b9dd748b75e68f433
WANG,SHI-HAO <41343107@nfu.edu.tw>
2025-12-22 15:14:31 +0800

    add

--- gtransform.h
+++ gtransform.h
@@ -2,6 +2,16 @@
 #define GTRANSFORM_H
 
 #include <QWidget>
+#include <QWidget>
+#include <QLabel>
+#include <QGroupBox>
+#include <QCheckBox>
+#include <QPushButton>
+#include <QDial>
+#include <QSpacerItem>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QImage>
 
 class gtransform : public QWidget
 {
@@ -10,5 +20,21 @@ class gtransform : public QWidget
 public:
     gtransform(QWidget *parent = nullptr);
     ~gtransform();
+    QLabel*inWin;
+    QGroupBox *mirrorGroup;
+    QCheckBox *hCheckBox;
+    QCheckBox *vCheckBox;
+    QPushButton *mirrorButton;
+    QDial *rotateDial;
+    QSpacerItem *vSpacer;
+    QHBoxLayout *mainLayout;
+    QVBoxLayout *groupLayout;
+    QVBoxLayout *leftLayout;
+    QImage srcImg;
+    QImage dstImg;
+
+private slots:
+    void mirroredImage();
+    void rotatedImage();
 };
 #endif // GTRANSFORM_H
